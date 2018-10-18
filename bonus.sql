0413d144a47a975a2addc2cbf8e7dcd78a2400d1
Matas Kazanavicius <mataskazanavicius@Matass-MacBook-Pro.local>
2018-10-19 00:26:34 +0300

    added items

--- bonus.sql
+++ bonus.sql
@@ -1,6 +1,6 @@
 --1.Straipsniai saugomi lentelėje News (newsId, text, date), straipsnio komentarai saugomi lentelėje Comments (commentId, text, date, newsId). Išrinkite 10 naujausių straipsnių su paskutiniu parašytu komentaru (newsId, newsDate, newsText, commentDate, commentText ).
 
-select n.newsId newsId, n.date newsDate, n.text newsText, max(c.date) commentDate, c.commentText commentText
+select n.newsId newsId, n.date newsDate, n.text newsText, c.date commentDate, c.commentText commentText
 from (select * from news order by date desc limit 10) n
 left join (select * from comments order by date desc limit 1) c
 on c.newsId = n.newsId;
